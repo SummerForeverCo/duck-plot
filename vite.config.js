8c6d312a868dae480969a3abc44ea320986430cc
mkfreeman <michael.k.freeman@gmail.com>
2024-08-06 15:35:19 -0400

    Clean up vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,5 @@
 import { defineConfig } from "vite";
-// import { nodePolyfills } from "vite-plugin-node-polyfills";
 import path from "path";
-
-// export default defineConfig({
-//   plugins: [nodePolyfills()],
-//   build: {
-//     lib: {
-//       entry: path.resolve(__dirname, "src/index.ts"),
-//       name: "PlotFit",
-//       fileName: (format) => `duck-plot.${format}`,
-//       formats: ["es", "cjs"],
-//     },
-//   },
-// });
-// // vite.config.js
-// import { defineConfig } from "vite";
-// import path from "path";
-// // TODO: Remove this dep if it doesn't work
-// import nodePolyfills from "vite-plugin-node-polyfills";
-// import NodePolyfillPlugin from "node-polyfill-webpack-plugin";
 
 export default defineConfig({
   test: {
@@ -28,20 +9,9 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
-      name: "PlotFit",
+      name: "DuckPlot",
       fileName: (format) => `duck-plot.${format}`,
       formats: ["es", "cjs"],
     },
   },
-  // rollupOptions: {
-  //   plugins: [NodePolyfillPlugin()],
-  // },
-  // // Enables node:* packages
-  // plugins: [nodePolyfills()],
-
-  // For running the dev
-  //   server: {
-  //     port: 8008,
-  //     open: "/",
-  //   },
 });
