c5fcd70478941c6afcbc06f30b8ccd3e830c4702
mkfreeman <michael.k.freeman@gmail.com>
2024-08-07 14:28:22 -0400

    Add dot type

---
+++ examples/plots/dot.js
@@ -0,0 +1,22 @@
+import { createDb } from "../util/createDb.js";
+export async function dot(duckplot) {
+  const plot = await createDb("income.csv")
+    .then(async (db) => {
+      const chart = duckplot
+        .data({ ddb: db, table: "income" })
+        .columns({
+          x: "execution_income",
+          y: "consensus_income",
+        })
+        .type("dot");
+
+      return await chart.plot();
+    })
+    .catch(console.error);
+  // for display
+  const codeString = `duckplot
+    .data({ ddb: db, table: "income" })
+    .columns({ x: "execution_incomr", y: "consensus_income" })
+    .type("dot")`;
+  return [plot, codeString];
+}
--- examples/plots/index.js
+++ examples/plots/index.js
@@ -1,2 +1,3 @@
 export * from "./bar.js";
 export * from "./line.js";
+export * from "./dot.js";
