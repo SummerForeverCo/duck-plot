592af4f1244bc5b56f627bb048ee7426718d7d04
mkfreeman <michael.k.freeman@gmail.com>
2024-09-03 11:35:34 -0400

    Add new examples

---
+++ examples/plots/fy.js
@@ -0,0 +1,14 @@
+import { renderPlot } from "../util/renderPlotClient.js";
+// This code is both displayed in the browser and executed
+const codeString = `
+duckplot
+  .table("income")
+  .x("month")
+  .y("consensus_income")
+  .fy("validator")
+  .color("validator", {type: "categorical"})
+  .options({width: 600, height: 800})
+  .type("line")
+`;
+
+export const fy = (options) => renderPlot("income.csv", codeString, options);
---
+++ examples/plots/groupedBarExplicit.js
@@ -0,0 +1,16 @@
+import { renderPlot } from "../util/renderPlotClient.js";
+// This code is both displayed in the browser and executed
+const codeString = `
+duckplot
+    .table("income")  
+    .query('SELECT *, validator::varchar as v FROM income WHERE validator in (2, 3, 4)')
+    .fx("month")
+  .y("consensus_income")
+  .x("v")
+  .color("v")
+  .type("barY")
+  .options({width: 300})
+`;
+
+export const groupedBarExplicit = (options) =>
+  renderPlot("income.csv", codeString, options);
