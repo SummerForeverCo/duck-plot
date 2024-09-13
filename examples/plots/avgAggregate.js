23898bdc221abf77d50508b0b2133e8fccc4aacd
mkfreeman <michael.k.freeman@gmail.com>
2024-09-12 20:38:34 -0400

    Add avg aggregate example

---
+++ examples/plots/avgAggregate.js
@@ -0,0 +1,15 @@
+import { renderPlot } from "../util/renderPlotClient.js";
+// This code is both displayed in the browser and executed
+
+const codeString = `duckplot
+  .table("income")
+  .x("month")
+  .y(["execution_income", "consensus_income"])
+  .mark("barY")
+  .config({
+    aggregate: "avg"
+  })
+  `;
+
+export const avgAggregate = (options) =>
+  renderPlot("income.csv", codeString, options);
