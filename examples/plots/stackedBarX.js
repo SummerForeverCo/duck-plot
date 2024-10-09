32ef36d12e3a9320a3ecf41b30a250b2445c170a
mkfreeman <michael.k.freeman@gmail.com>
2024-10-09 10:38:21 -0400

    Add percentage stack examples

---
+++ examples/plots/stackedBarX.js
@@ -0,0 +1,13 @@
+import { renderPlot } from "../util/renderPlotClient.js";
+// This code is both displayed in the browser and executed
+
+const codeString = `duckplot
+  .table("income")
+  .x(["consensus_income", "execution_income"], {label: "Percent income", percent: true})
+  .y("month", {label: "This long label should be truncated in the tooltip"})
+  .mark("barX", {offset: "normalize"})
+  .options({height: 500})
+  `;
+
+export const stackedBarX = (options) =>
+  renderPlot("income.csv", codeString, options);
